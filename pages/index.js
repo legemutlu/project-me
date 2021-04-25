5dddd9f1a167f6b62e1680411f433947661a9367
lege <legemutlu@gmail.com>
2021-04-25 22:58:02 +0300

    feat: home page style changed

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,19 @@
 export default function HomePage() {
-  return <div className="text-9xl">Welcome</div>
+  return (
+    <>
+      <div className="site-container">
+        <div className="space-y-4">
+          <h1 className="text-3xl font-bold">Merhaba</h1>
+          <p>
+            Ben Levent Ege Mutlu. Yazılım, felsefe ve sanat konularıyla yakından
+            ilgileniyorum.
+          </p>
+        </div>
+      </div>
+
+      <div className="max-w-4xl mx-auto mt-20">
+        <img src="31193.jpeg" alt="" />
+      </div>
+    </>
+  )
 }
