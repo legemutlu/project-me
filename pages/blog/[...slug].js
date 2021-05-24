10722e0eb565259528a7bc0c492368c9549897fb
lege <legemutlu@gmail.com>
2021-05-24 22:40:06 +0300

    feat: added post page

---
+++ pages/blog/[...slug].js
@@ -0,0 +1,43 @@
+import { getMdxNode, getMdxPaths } from 'next-mdx/server'
+import { useHydrate } from 'next-mdx/client'
+import { mdxComponents } from '../../components/mdx-components'
+
+export default function PostPage({ post }) {
+
+  const content = useHydrate(post, {
+    components: mdxComponents
+  })
+  return (
+    <div className="site-container">
+      <article className="prose">
+        <h1>{post.frontMatter.title}</h1>
+        <p>{post.frontMatter.excerpt}</p>
+        <hr/>
+        <div>{content}</div>
+      </article>
+    </div>
+  )
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: await getMdxPaths('post'),
+    fallback: false
+  }
+}
+
+export async function getStaticProps(context) {
+  const post = await getMdxNode('post', context)
+
+  if (!post) {
+    return {
+      notFound: true
+    }
+  }
+
+  return {
+    props: {
+      post
+    }
+  }
+}
