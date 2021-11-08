75a3bdd0c4c77536914b541776f37dfb1d1cd808
lege <legemutlu@gmail.com>
2021-11-08 19:26:41 +0300

    feat: theme header

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,6 +27,9 @@ module.exports = {
             '*': {
               color: 'inherit !important',
             },
+            code:{
+              color: 'white !important',
+            }
           },
         },
         dark: {
